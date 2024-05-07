3f5cf8a08192d89ebbb7359a195a773bf9872310
ecaleb97 <valley_rob.0c@icloud.com>
2024-05-07 22:00:10 +0200

    Update border color in ExperienceList component

--- src/components/experience/experience-list.tsx
+++ src/components/experience/experience-list.tsx
@@ -3,7 +3,7 @@ import { ExperienceItem } from '@/components/experience/experience-item';
 export function ExperienceList() {
   return (
     <ol
-      className='relative space-y-6 border-s-2 border-neutral-300 
+      className='relative space-y-6 border-s-2 border-neutral-400 
       dark:border-neutral-600'
     >
       <ExperienceItem />
