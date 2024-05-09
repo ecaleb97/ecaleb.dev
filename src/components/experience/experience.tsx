b0fd7a4a5e5d18840b520c034aa85dcf25b056b3
ecaleb97 <valley_rob.0c@icloud.com>
2024-05-09 02:49:25 +0200

    Update title translations in messages files

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,16 +1,19 @@
 import { ExperienceList } from '@/components/experience/experience-list';
 import { SectionHeading } from '@/components/heading/section-heading';
 import { SectionWrapper } from '@/components/section/section-wrapper';
+import { useTranslations } from 'next-intl';
 
 
 export function Experience() {
+  const t = useTranslations('experience');
+
   return (
     <SectionWrapper
       id="experience"
       className='mb-20'
     >
       <SectionHeading>
-        Experience
+        {t('title')}
       </SectionHeading>
       <ExperienceList />
     </SectionWrapper>
