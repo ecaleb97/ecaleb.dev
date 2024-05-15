3e4aca23ca7167a84b9ad68a116b2da9568f8c03
ecaleb97 <valley_rob.0c@icloud.com>
2024-05-15 14:29:44 +0200

    refactor: Update header styles for better visibility and accessibility

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -48,7 +48,8 @@ export function Header () {
         /> */}
         <nav 
           className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2
-          py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'
+          py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 bg-[#fafafa]/90
+          dark:bg-black/90 rounded-full px-4'
         >
           <ul
             className='flex w-[22rem] items-center justify-center
