6ff09fcb8cf5d391bfe5710f3792e012d76acc33
ecaleb97 <valley_rob.0c@icloud.com>
2024-05-16 12:57:02 +0200

    improve message in error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { useEffect } from 'react';
 
 export default function Error({
@@ -15,8 +14,8 @@ export default function Error({
   }, [error]);
 
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center p-4'>
-      <h2 className='text-2xl'>Oh no, something went wrong... maybe refresh?</h2>
+    <main className='min-h-screen w-full flex flex-col gap-y-6 items-center justify-center p-4'>
+      <h2 className='text-2xl text-center'>Oh no, something went wrong... maybe refresh?</h2>
       <button className='text-lg' onClick={() => reset()}>Try again</button>
     </main>
   );
