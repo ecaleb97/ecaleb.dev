084603c775511c1cde3daa8737a36936ebf420d3
ecaleb97 <valley_rob.0c@icloud.com>
2024-06-05 12:28:49 +0200

    refactor: Add id prop to SectionWrapper component

--- src/components/section/section-wrapper.tsx
+++ src/components/section/section-wrapper.tsx
@@ -1,12 +1,14 @@
 import { SectionWrapperProps } from '@/types/types';
 
+
 export function SectionWrapper({ 
-  children, className
+  children, className, id
 }: SectionWrapperProps) {
   return (
     <section
       className={`w-full max-w-[42rem] text-left
       scroll-mt-28 ${className} flex flex-col gap-6`}
+      id={id}
     >
       {children}
     </section>
