8ac4c5c3933212dfad9ec9c1116d16b2e8da0ac9
ecaleb97 <valley_rob.0c@icloud.com>
2024-06-15 19:49:58 +0200

    refactor: Update blog page metadata and description

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,13 +3,21 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Blog',
-  description: 'Read my thoughts and experiences in the tech industry.',
+  description: 'I write about web development, other topics and occasionally life updates!',
+  openGraph: {
+    title: 'Blog',
+    description: 'I write about web development, other topics and occasionally life updates!',
+    type: 'website',
+    url: 'https://ecaleb.dev/blog'
+  }
 };
 
 export default function BlogPage() {
+
   return (
     <main className='flex justify-center items-center px-5'>
       <Blog />
     </main>
   );
 }
+
