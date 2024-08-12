7a884f1a8d9df691c2e1a53f0913730807aa02b1
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-12 20:53:21 +0200

    add featured projects in main page

--- src/components/heading/section-heading.tsx
+++ src/components/heading/section-heading.tsx
@@ -1,7 +1,7 @@
 export function SectionHeading({ children }: { children: React.ReactNode }) {
 	return (
-		<h1 className="text-3xl font-medium capitalize tracking-wider">
+		<h2 className="text-3xl font-medium capitalize tracking-wider">
 			{children}
-		</h1>
+		</h2>
 	);
 }
